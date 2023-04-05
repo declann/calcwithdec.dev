94c673e0f4ec2813cb96ea3d97b85db02576c53d
Declan Naughton <declan.naughton2@mail.dcu.ie>
2023-04-05 14:04:40 -0300

    move ohq script to use deno (better security)

--- tools/model-viz/ohq/update-DO-NOT-RUN.sh
+++ tools/model-viz/ohq/update-DO-NOT-RUN.sh
@@ -1,11 +1,13 @@
 # Do not run this without trusting completely the ObservableHQ API, the tarball and contents which are run in node, and this script
 # There is no warranty!
 
+# This script works for me, it probably won't work for you without changing the deno location
+
 wget https://api.observablehq.com/@declann/calculang-tools-bad.tgz?v=3
 
 tar -zxvf ./calculang-tools-bad.tgz?v=3 ./package.json
 
-main=$(node -e "const p = require('./package.json'); console.log(p.main)")
+main=$(~/.deno/bin/deno eval "import p from './package.json' assert { type: 'json' }; console.log(p.main)")
 
 tar -zxvf ./calculang-tools-bad.tgz?v=3 ./$main
 
